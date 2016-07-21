ac2614810000ddd27546cdbd108284a9603fd74b
sahithirukanana <sahithi.rukanana9@gmail.com>
2016-07-21 19:18:34 +0200

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append("Sahithi Rukanana");
+ var awesomethoughts;
+ awesomethoughts = "I am Sahithi and I am Awesome";
+ console.log(awesomethoughts);
